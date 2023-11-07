b85db6f5db73d859d8cb333a304f29e2d1bbe7bd
CollinpBrock <123254643+CollinpBrock@users.noreply.github.com>
2023-11-07 16:09:07 -0600

    set up structs

--- huff.cpp
+++ huff.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+
 using namespace std;
+
+
+
+struct HuffTable
+{
+    int glyph = -1;
+    int frequency = 0;
+    int leftP = -1;
+    int rightP = -1;
+};
+
+struct FileInformation 
+{
+    string fileName;
+    int fileNameLength;
+    ifstream fileStream;
+    double fileStreamLength;
+    int glyphsInFile;
+};
+
+
 
 int main() {
     cout << "Hello, world" << endl;
