439f2fc26a5f3b067b947cdbffe34647634c7235
Joshua Sutton <85507895+jshutone@users.noreply.github.com>
2023-11-07 18:57:21 -0600

    Added pseudo code

--- huff.cpp
+++ huff.cpp
@@ -25,5 +25,19 @@ struct FileInformation
 
 
 int main() {
-    cout << "Hello, world" << endl;
+    // get filename
+    FileInformation newFile;
+    cout << "Filename to compress: ";
+    cin >> newFile.fileName
+    
+
+
+    // Length of sourcefilename, sourcefilename, and then number of huffman entries
+
+    // 1. get freq and gl 4. yphs from file
+    // 2. then sort array smallest to largest
+    // 3. mark 'H' and 'M'
+    // 4. follow instrucitons on canvas
+    // 5. create reheap function to go through all entries to make sure the lowest are in 1 and 2
+    // 6. crearte function to write each element in the array to the .huf file.
 }
