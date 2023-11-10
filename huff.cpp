ba25d0b8e2ec91abd8830f6ec66fe0f0e1321ae5
Joshua Sutton <joshuajaysutton@gmail.com>
2023-11-10 08:42:20 -0600

    Added TODO list
    Lets start knocking some of these things out!

--- huff.cpp
+++ huff.cpp
@@ -1,3 +1,25 @@
+// THINGS TODO:
+// 1.0 Need to open and read data from file
+//  1.1: prompt user what file to open
+//  1.2: open that file with a filestream object
+//  1.3: create a new file with a '.huf. extenstion to write the compressed data
+//  1.4: get the glyphs and freq from the data and put it in an array
+// 2.0 writting to '.huf' file
+//  2.1: get lenght of sourcefilename
+//  2.2: write sourcefilename
+//  2.3: write number of entries
+//  2.4: write the huffman table (could be from an array)
+//  2.5: write the compressed data
+// 3.0 Huffman Table:
+//  3.1 write merge algortim
+//  3.2 write reheap algortim
+//  3.3 put each node in the array
+// 4.0:The compressed data:
+//  4.1 IDK how to write the compressed data
+
+
+
+
 #include <iostream>
 
 using namespace std;
